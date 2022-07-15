2956b62924d0bdadda1776a85a8f139c5bba446d
10Dev <today13113@gmail.com>
2022-07-15 17:45:36 +0700

    update[Dice Roller]

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.diceroller
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,14 +14,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
+        val textView: TextView = findViewById(R.id.textView)
         rollDice()
+        textView.text = getString(R.string.start_text)
         rollButton.setOnClickListener { rollDice() }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val luckyNum = dice.luckyNum()
         val diceImage: ImageView = findViewById(R.id.imageView)
+        val textView: TextView = findViewById(R.id.textView)
         val drawableResource = when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -27,6 +35,9 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
+        if (diceRoll == luckyNum){
+            textView.text = "You are lucky"
+        }else textView.text = "bad luck,try again"
         diceImage.setImageResource(drawableResource)
         diceImage.contentDescription = diceRoll.toString()    }
 }
@@ -34,6 +45,9 @@ class MainActivity : AppCompatActivity() {
 class Dice(private val numSides: Int) {
 
     fun roll(): Int {
+        return (1..numSides).random()
+    }
+    fun luckyNum():Int{
         return (1..numSides).random()
     }
 }
